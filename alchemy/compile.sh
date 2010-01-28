3b0176f1a98d5fc002d61b14e1bb51f4db8bdb20
U-PRODUCER\Checkers <Checkers@producer.(none)>
2010-01-28 21:57:47 +0100

    Changed paths in compile script to explicit paths

--- alchemy/compile.sh
+++ alchemy/compile.sh
@@ -1,1 +1,1 @@
-gcc -Ilibdns_sd/include -Llibdns_sd/lib/ -llibdns_sd -O3 -Wall as3dns_sd.c -swc -o as3dns_sd.swc
+gcc -I./libdns_sd/include -Llibdns_sd/lib/ -llibdns_sd -O3 -Wall as3dns_sd.c -swc -o as3dns_sd.swc
